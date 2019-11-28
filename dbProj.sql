e905b5b933a65b6e034c56623e8c95f229c8b114
Aaron Thompson <athompson812@hotmail.com>
2019-11-27 21:52:12 -0800

    fixed sql errors in dbProj.sql

--- dbProj.sql
+++ dbProj.sql
@@ -1,4 +1,4 @@
-RAGMA foreign_keys= ON;
+PRAGMA foreign_keys= ON;
 drop table if exists Employee;
 drop table if exists Department;
 drop table if exists SalaryScale;
@@ -8,16 +8,18 @@ drop table if exists EmployeePositionInformationByYear;
 Create table Employee(
 	upsID Integer Primary Key,
 	lastName Text not null,
-	firstName Text not null)
-    type Text check(type='T' or type='I' or type='CL' or type='S' or type='E' or type like 'VAP%' or type like 'VIN%');
+	firstName Text not null,
+    type Text check(type='T' or type='I' or type='CL' or type='S' or type='E' or type like 'VAP%' or type like 'VIN%')
+);
 Create table Department(
 	deptID Text Primary Key check(length(deptID)<=4),
-	deptName Text Unique Not Null,);
+	deptName Text Unique Not Null);
 Create table SalaryScale(
 	rank Text check(rank='Asst' or rank='Assc' or rank='Full' or rank='CLAsst' or rank='CLAssc'),
 	step Integer,
 	baseSalary Integer,
-	Primary Key (rank,step);
+	Primary Key (rank,step)
+);
 Create table SalaryAdjustments(
 	adjID Text Primary Key,
 	adjVal Real Not Null,
@@ -28,12 +30,12 @@ Create table EmployeePositionInformationByYear(
     year Integer check(year>1950) not Null,
     upsID Integer not Null,
     positionNumber Integer not Null,
-    includeNext Text check(toNext='tenative' or toNext='yes' or toNext='no'),
+    includeNext Text check(includeNext='tenative' or includeNext='yes' or includeNext='no'),
     rank Text not null,
     step Integer not null,
     stepYear Integer not null,
     Primary Key(year,upsID),
-    Foreign Key (rank,step) References SalaryScale(rank,step),
+    Foreign Key (rank,step) References SalaryScale(rank,step)
         On Update Cascade
     Foreign Key (upsID) References Employee(upsID)
         On Update Cascade
@@ -45,9 +47,10 @@ Create table EmployeeAdjustments(
 	adjID Text not Null,
     Primary Key (year,upsID,adjID),
     Foreign Key (year,upsID) References EmployeePositionInformationByYear(year,upsID),
-    Foreign Key (adjID)
+    Foreign Key (adjID) References SalaryAdjustments(adjID)
         On Update Cascade
-        On Delete Cascade);
+        On Delete Cascade
+);
 
 
    
